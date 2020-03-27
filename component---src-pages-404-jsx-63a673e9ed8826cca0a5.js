b9d5163a59bab6d29f3f2b4e98510f26f78c1b67
yld-weng <59968766+yld-weng@users.noreply.github.com>
2020-03-27 17:37:59 +0000

    Updates

--- component---src-pages-404-jsx-63a673e9ed8826cca0a5.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{pssB:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),s=a.n(n),i=a("L12J"),u=a("dmsj");t.default=function(){return s.a.createElement(i.a,null,s.a.createElement(u.a,{title:"404 Not found",keywords:["the university of sheffield","data visualisation","data visualisation hub","research"]}),s.a.createElement("h1",null,"NOT FOUND"),s.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-jsx-63a673e9ed8826cca0a5.js.map

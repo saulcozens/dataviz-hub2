5244a0bae7d1034cfaf8e57f8ac223ce4fbd0fa2
Travis CI User <travis@example.org>
2020-04-27 17:02:00 +0000

    Updates

--- 5e2a4920-74222adb62b2c0c598ba.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{Tgqd:function(t,n,r){"use strict";r.d(n,"a",(function(){return o}));var e=r("Lnxd"),o=function(t){return Object(e.a)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"circle",attr:{cx:"11",cy:"11",r:"8"}},{tag:"line",attr:{x1:"21",y1:"21",x2:"16.65",y2:"16.65"}}]})(t)};o.displayName="FiSearch"}}]);
-//# sourceMappingURL=5e2a4920-74222adb62b2c0c598ba.js.map
--- styles-e8282917bc5540453891.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],[]]);
-//# sourceMappingURL=styles-e8282917bc5540453891.js.map

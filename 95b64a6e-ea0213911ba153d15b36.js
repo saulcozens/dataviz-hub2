e71ab9b01b5f120a3fc36a367767f1a2bec4e3cb
Travis CI User <travis@example.org>
2020-05-21 15:44:56 +0000

    Updates

--- 95b64a6e-ea0213911ba153d15b36.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{IdFE:function(t,n,a){"use strict";a.d(n,"a",(function(){return o}));var c=a("Lnxd"),o=function(t){return Object(c.a)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M256 217.9L383 345c9.4 9.4 24.6 9.4 33.9 0 9.4-9.4 9.3-24.6 0-34L273 167c-9.1-9.1-23.7-9.3-33.1-.7L95 310.9c-4.7 4.7-7 10.9-7 17s2.3 12.3 7 17c9.4 9.4 24.6 9.4 33.9 0l127.1-127z"}}]})(t)};o.displayName="IoIosArrowUp"}}]);
-//# sourceMappingURL=95b64a6e-ea0213911ba153d15b36.js.map
